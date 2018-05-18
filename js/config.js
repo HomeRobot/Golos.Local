f00baa15bf8d2da244dee2e640962bf2c4e0c83c
HomeRobot <alex.bakulin@gmail.com>
2018-05-18 17:31:57 +0300

    Add files via upload
    Base JS

---
+++ js/config.js
@@ -0,0 +1,7 @@
+//golos.config.set('websocket','wss://api.golos.cf');
+golos.config.set('websocket','wss://ws.golos.io/');
+//golos.config.set('websocket','wss://ws17.golos.blog');
+//golos.config.set('websocket',"wss://api.golos.blckchnd.com/ws");
+//golos.config.set('address_prefix','GLS');
+//golos.config.set('chain_id','5876894a41e6361bde2e73278f07340f2eb8b41c2facd29099de9deef6cdb679');
+
